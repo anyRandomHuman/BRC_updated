fix normalization

jaxrl/agent/update.py
@@ -30,79 +30,30 @@ def update_actor(key: PRNGKey, actor: Model, critic: Model, temp: Model, batch: 
     info['actor_gnorm'] = info.pop('grad_norm')
     return new_actor, info
 
-def update_critic_old(key: PRNGKey, actor: Model, critic: Model, target_critic: Model,
-           temp: Model, batch: Batch, discount: float, num_bins: int, v_max: float, multitask: bool):
-    
-    inputs = build_actor_input(critic, batch.next_observations, batch.task_ids, multitask)
-    dist = actor(inputs)
-    next_actions, next_log_probs = dist.sample_and_log_prob(seed=key)
-    next_q1_logits, next_q2_logits = target_critic(batch.next_observations, next_actions, batch.task_ids)
-    next_q1_probs = jax.nn.softmax(next_q1_logits, axis=-1)
-    next_q2_probs = jax.nn.softmax(next_q2_logits, axis=-1)
-    next_q_probs = (next_q1_probs + next_q2_probs) / 2
-    # compute target value buckets
-    v_min = -v_max
-    bin_values = jnp.linspace(start=v_min, stop=v_max, num=num_bins)[None]
-    target_bin_values = batch.rewards[:, None] + discount * batch.masks[:, None] * (bin_values - temp() * next_log_probs[:, None])
-    target_bin_values = jnp.clip(target_bin_values, v_min, v_max)
-    target_bin_values = (target_bin_values - v_min) / ((v_max - v_min) / (num_bins - 1))
-    lower, upper = jnp.floor(target_bin_values), jnp.ceil(target_bin_values)
-    lower_mask = jax.nn.one_hot(lower.reshape(-1), num_bins).reshape((-1, num_bins, num_bins))
-    upper_mask = jax.nn.one_hot(upper.reshape(-1), num_bins).reshape((-1, num_bins, num_bins))
-    lower_values = (next_q_probs * (upper + (lower == upper).astype(jnp.float32) - target_bin_values))[..., None]        
-    upper_values = (next_q_probs * (target_bin_values - lower))[..., None]
-    target_probs = jax.lax.stop_gradient(jnp.sum(lower_values * lower_mask + upper_values * upper_mask, axis=1))
-    q_value_target = (bin_values * target_probs).sum(-1)
-
-    def critic_loss_fn(critic_params: Params):
-        critic_fn = lambda actions: critic.apply({"params": critic_params}, batch.observations, actions, batch.task_ids)
-        def _critic_fn(actions):
-            q1, q2 = critic_fn(actions)
-            return 0.5 * (q1 + q2).mean(), (q1, q2)
-        (_, (q1_logits, q2_logits)), action_grad = jax.value_and_grad(_critic_fn, has_aux=True)(batch.actions)
-        q1_logprobs = jax.nn.log_softmax(q1_logits, axis=-1)
-        q2_logprobs = jax.nn.log_softmax(q2_logits, axis=-1)
-        
-        loss1 = -jnp.mean(jnp.sum(target_probs * q1_logprobs, axis=1))
-        loss2 = -jnp.mean(jnp.sum(target_probs * q2_logprobs, axis=1))
-        critic_loss = loss1 + loss2
-        
-        return critic_loss, {
-            "critic_loss": critic_loss,
-            "q_mean": q_value_target.mean(),
-            "q_min": q_value_target.min(),
-            "q_max": q_value_target.max(),
-            "r": batch.rewards.mean(),
-            "critic_pnorm": tree_norm(critic_params),
-            "critic_agnorm": jnp.sqrt((action_grad**2).sum(-1)).mean(0),
-        }
-    new_critic, info = critic.apply_gradient(critic_loss_fn)
-    info["critic_gnorm"] = info.pop("grad_norm")
-    return new_critic, info
-
-
 def update_critic(key: PRNGKey, actor: Model, critic: Model, target_critic: Model,
            temp: Model, batch: Batch, discount: float, num_bins: int, v_max: float, multitask: bool):
-    
     inputs = build_actor_input(critic, batch.next_observations, batch.task_ids, multitask)
     dist = actor(inputs)
     next_actions, next_log_probs = dist.sample_and_log_prob(seed=key)
     next_q_logits = target_critic(batch.next_observations, next_actions, batch.task_ids)
     next_q_probs = jax.nn.softmax(next_q_logits, axis=-1).mean(axis=0)
-    # compute target value buckets
     v_min = -v_max
     bin_values = jnp.linspace(start=v_min, stop=v_max, num=num_bins)[None]
+    
+    delta_z = ((v_max - v_min) / (num_bins - 1))
     target_bin_values = batch.rewards[:, None] + discount * batch.masks[:, None] * (bin_values - temp() * next_log_probs[:, None])
     target_bin_values = jnp.clip(target_bin_values, v_min, v_max)
-    target_bin_values = (target_bin_values - v_min) / ((v_max - v_min) / (num_bins - 1))
+    target_bin_values = (target_bin_values - v_min) / delta_z
+    
     lower, upper = jnp.floor(target_bin_values), jnp.ceil(target_bin_values)
     lower_mask = jax.nn.one_hot(lower.reshape(-1), num_bins).reshape((-1, num_bins, num_bins))
     upper_mask = jax.nn.one_hot(upper.reshape(-1), num_bins).reshape((-1, num_bins, num_bins))
+    
     lower_values = (next_q_probs * (upper + (lower == upper).astype(jnp.float32) - target_bin_values))[..., None]        
     upper_values = (next_q_probs * (target_bin_values - lower))[..., None]
+    
     target_probs = jax.lax.stop_gradient(jnp.sum(lower_values * lower_mask + upper_values * upper_mask, axis=1))
     q_value_target = (bin_values * target_probs).sum(-1)
-
     def critic_loss_fn(critic_params: Params):
         q_logits = critic.apply({"params": critic_params}, batch.observations, batch.actions, batch.task_ids)
         q_logprobs = jax.nn.log_softmax(q_logits, axis=-1)
@@ -114,12 +65,10 @@ def update_critic(key: PRNGKey, actor: Model, critic: Model, target_critic: Mode
             "q_max": q_value_target.max(),
             "r": batch.rewards.mean(),
             "critic_pnorm": tree_norm(critic_params),
-            #"critic_agnorm": jnp.sqrt((action_grad**2).sum(-1)).mean(0),
         }
     new_critic, info = critic.apply_gradient(critic_loss_fn)
     info["critic_gnorm"] = info.pop("grad_norm")
     return new_critic, info
-
 
 def update_target_critic(critic: Model, target_critic: Model, tau: float):
     new_target_params = jax.tree.map(
@@ -137,6 +86,8 @@ def update_temperature(temp: Model, entropy: float, target_entropy: float):
     return new_temp, info
 
 '''
+from jaxrl.utils import Batch
+
 key = agent.rng
 actor = agent.actor
 target_critic = agent.target_critic

jaxrl/envs.py
@@ -17,7 +17,10 @@ class FlattenObservationShadowhandWrapper(gym.ObservationWrapper):
         return np.concatenate((obs['observation'], obs['desired_goal']), axis=-1)
 
 def _make_env_dmc(env_name: str, seed: int = 0) -> gym.Env:
-    from dmc_envs.tasks import cheetah, walker, hopper, reacher, ball_in_cup, pendulum, fish
+    try:
+        from dmc_envs.tasks import cheetah, walker, hopper, reacher, ball_in_cup, pendulum, fish
+    except:
+        print('Could not load additional DMC tasks')
     from dm_control import suite
     suite.ALL_TASKS = suite.ALL_TASKS + suite._get_tasks('custom')
     suite.TASKS_BY_DOMAIN = suite._get_tasks_by_domain(suite.ALL_TASKS)

jaxrl/networks.py
@@ -83,10 +83,10 @@ class QValueEnsemble(nn.Module):
         q_values = self.q_value_ensemble(inputs)
         return q_values
     
-class DoubleCriticTest(nn.Module):
+class Critic(nn.Module):
     num_tasks: int
     embedding_size: int
-    ensemble_size: int
+    ensemble_size: int = 2
     hidden_dims: int = 512
     depth: int = 2
     activations: Callable[[jnp.ndarray], jnp.ndarray] = nn.relu
@@ -114,43 +114,6 @@ class DoubleCriticTest(nn.Module):
             inputs = jnp.concatenate((observations, actions, task_embedding), axis=-1)            
         q_values = self.q_value_ensemble(inputs)
         return q_values
-    
-class DoubleCritic(nn.Module):
-    num_tasks: int
-    embedding_size: int
-    hidden_dims: int = 512
-    depth: int = 2
-    activations: Callable[[jnp.ndarray], jnp.ndarray] = nn.relu
-    output_nodes: int = 101
-    multitask: bool = False
-    
-    def setup(self):
-        if self.multitask:
-            self.task_embedding = TaskEmbedding(self.num_tasks, self.embedding_size)
-        self.critic1 = QValue(
-            self.hidden_dims, 
-            self.depth,
-            activations=self.activations,
-            output_nodes=self.output_nodes,
-        )
-        self.critic2 = QValue(
-            self.hidden_dims, 
-            self.depth,
-            activations=self.activations,
-            output_nodes=self.output_nodes,
-        )
-
-    def __call__(self, observations: jnp.ndarray, actions: jnp.ndarray, task_ids: jnp.ndarray, return_embeddings: bool = False):
-        if self.multitask is False:
-            inputs = jnp.concatenate((observations, actions), axis=-1)
-        else:
-            task_embedding = self.task_embedding(task_ids)
-            if return_embeddings:
-                return task_embedding
-            inputs = jnp.concatenate((observations, actions, task_embedding), axis=-1)            
-        q_value1 = self.critic1(inputs)
-        q_value2 = self.critic2(inputs)
-        return q_value1, q_value2
 
 class NormalTanhPolicy(nn.Module):
     action_dim: int

jaxrl/normalizer.py
@@ -1,69 +1,54 @@
 import numpy as np
 from jaxrl.utils import Batch
 
+class RunningMeanStd:
+    """Tracks the mean, variance and count of values."""
+    # https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Parallel_algorithm
+    def __init__(self, epsilon=1e-4, shape=(), dtype=np.float32):
+        """Tracks the mean, variance and count of values."""
+        self.mean = np.zeros(shape, dtype=dtype)
+        self.var = np.ones(shape, dtype=dtype)
+        self.count = epsilon
+
+    def update(self, x):
+        """Updates the mean, var and count from a batch of samples."""
+        batch_mean = np.mean(x, axis=0)
+        batch_var = np.var(x, axis=0)
+        batch_count = x.shape[0]
+        self.update_from_moments(batch_mean, batch_var, batch_count)
+
+    def update_from_moments(self, batch_mean, batch_var, batch_count):
+        """Updates from batch mean, variance, and count moments."""
+        delta = batch_mean - self.mean
+        tot_count = self.count + batch_count
+        ratio = batch_count / tot_count
+        new_mean = self.mean + delta * ratio
+        m_a = self.var * self.count
+        m_b = batch_var * batch_count
+        M2 = m_a + m_b + np.square(delta) * self.count * ratio
+        new_var = M2 / tot_count
+        self.mean = new_mean
+        self.var = new_var
+        self.count = tot_count
+
 class RewardNormalizer(object):
-    def __init__(self, num_seeds: int, target_entropy: float, discount: float = 0.99, v_max: float = 10.0, max_steps: int | None = None):
-        self.returns_min_norm = np.zeros(num_seeds) + np.inf
-        self.returns_max_norm = np.zeros(num_seeds) - np.inf           
-        self.effective_horizon = 1 / (1 - discount)
+    def __init__(self, num_tasks: int, discount: float = 0.99, v_max: float = 10.0):
+        self.returns = np.zeros(num_tasks, dtype=np.float32)
+        self.returns_max = np.zeros(num_tasks, dtype=np.float32)
+        self.returns_rms = RunningMeanStd(shape=num_tasks, dtype=np.float32)
         self.discount = discount
         self.v_max = v_max
-        self.target_entropy = target_entropy        
-        self.max_steps = max_steps
-        self.step = 0
-        self.rewards = np.zeros((num_seeds, max_steps)) if max_steps is not None else [[] for _ in range(num_seeds)]
-        
-    def _calculate_returns_variable_length_trajectory(self, rewards_traj: list, truncate: bool):
-        values = np.zeros_like(rewards_traj)
-        bootstrap = rewards_traj.mean() * self.effective_horizon if truncate else 0.0
-        for i in reversed(range(rewards_traj.shape[0])):
-            values[i] = rewards_traj[i] + self.discount * bootstrap
-            bootstrap = values[i]
-        return values.min(axis=-1), values.max(axis=-1)
-    
-    def _calculate_returns_fixed_length_trajectory(self):
-        values = np.zeros_like(self.rewards)
-        bootstrap = self.rewards.mean(-1) * self.effective_horizon
-        for i in reversed(range(values.shape[-1])):
-            values[:, i] = self.rewards[:, i] + self.discount * bootstrap
-            bootstrap = values[:, i]
-        return values.min(axis=-1), values.max(axis=-1)
-        
-    def _update_variable_length_trajectory(self, rewards: np.ndarray, terminal: np.ndarray, truncate: np.ndarray):
-        for i, reward in enumerate(rewards):
-            self.rewards[i].append(reward)
-        done = np.logical_or(terminal, truncate)
-        if done.any():
-            indx = done.nonzero()[0]
-            for j in indx:
-                rewards_traj = np.asarray(self.rewards[j])
-                value_min, value_max = self._calculate_returns_variable_length_trajectory(rewards_traj, truncate[j])
-                self.returns_min_norm[j] = min(self.returns_min_norm[j], value_min) 
-                self.returns_max_norm[j] = max(self.returns_max_norm[j], value_max) 
-                self.rewards[j] = []
-                
-    def _update_fixed_length_trajectory(self, rewards: np.ndarray, terminal: np.ndarray, truncate: np.ndarray):
-        self.rewards[:, self.step] = rewards
-        dones = np.logical_or(terminal, truncate)
-        if self.step == self.max_steps - 1:
-            assert dones.all()
-            v_min, v_max = self._calculate_returns_fixed_length_trajectory()
-            self.returns_min_norm = np.where(v_min < self.returns_min_norm, v_min, self.returns_min_norm)
-            self.returns_max_norm = np.where(v_max > self.returns_max_norm, v_max, self.returns_max_norm)            
-            self.step = 0
-        else:
-            self.step += 1
-        
-    def update(self, rewards: np.ndarray, terminal: np.ndarray, truncate: np.ndarray):
-        if self.max_steps is not None:
-            self._update_fixed_length_trajectory(rewards, terminal, truncate)
-        else:
-            self._update_variable_length_trajectory(rewards, terminal, truncate)
-            
-    def normalize(self, batches: Batch, temperature: np.ndarray):
-        denominator = np.where(self.returns_max_norm > np.abs(self.returns_min_norm), self.returns_max_norm, np.abs(self.returns_min_norm))
-        denominator = (denominator - temperature * self.effective_horizon * self.target_entropy / 2) / self.v_max
+
+    def normalize(self, batches: Batch):
+        var_denominator = np.sqrt(self.returns_rms.var + 1e-8)
+        min_required_denominator = self.returns_max / self.v_max
+        denominator = np.where(var_denominator > min_required_denominator, var_denominator, min_required_denominator)
         denominator = denominator[batches.task_ids]
         rewards = batches.rewards / denominator
         return Batch(observations=batches.observations, actions=batches.actions, rewards=rewards, masks=batches.masks, next_observations=batches.next_observations, task_ids=batches.task_ids)
-   
+
+    def update(self, rewards: np.ndarray, terminal: np.ndarray, truncate: np.ndarray):
+        done = np.logical_or(terminal, truncate)
+        self.returns = self.discount * (1 - done) * self.returns + rewards
+        self.returns_rms.update(self.returns[None])
+        self.returns_max = np.where(self.returns_max > np.absolute(self.returns), self.returns_max, np.absolute(self.returns))